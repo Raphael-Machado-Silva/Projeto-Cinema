37d42b2d55a0afb8a0b93713d401b7f6dfd9a675
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-11-06 11:53:30 -0300

    update site cinema

--- cine/src/App.jsx
+++ cine/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Link, Outlet } from "react-router-dom";
+import logo from './assets/logo.webp';  // Importe a imagem aqui
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      <nav id='navbar'>
+        <h2>
+          <img 
+            src={logo} 
+            alt="CineStream Logo" 
+            style={{
+              width: '55px',   // Ajuste o tamanho desejado
+              height: '55px',  // Ajuste para a altura ser igual à largura
+              borderRadius: '50%',  // Torna a imagem circular
+              objectFit: 'cover',  // Garante que a imagem não distorça
+            }} 
+          />
+          <Link to="/">CineStream</Link>
+        </h2>
+        <Link to="/movie/1">Filmes</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+
+      <h2>CineStream</h2>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- cine/src/main.jsx
+++ cine/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import App from './App.jsx'
+import Movie from './pages/Movie.jsx'
+import Search from './pages/Search.jsx'
+import Home from './pages/Home.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App></App>}>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="movie/:id" element={<Movie></Movie>}></Route>
+            <Route path="search" element={<Search></Search>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
