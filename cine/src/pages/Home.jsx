922c2118b36dd311019abd8036feb7c8c92abeb9
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-11-07 10:07:49 -0300

    Update Home.jsx

--- cine/src/pages/Home.jsx
+++ cine/src/pages/Home.jsx
@@ -4,9 +4,27 @@ const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(()=>{ // carregar filmes quando a pag for carregada
+    // montando a URL para fzr isso
+
+    const topRatedUrl = `${moviesURL}top_rated?api_key=${apiKey}&language=pt-BR`
+    console.log(apiKey)
+    getTopRatedMovies(topRatedUrl) // enviando a URL para função que pega os filmes
+
+  }, [])
+
   return (
     <div>
-      Home
+      {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
     </div>
   )
 }
